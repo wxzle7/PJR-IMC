585af34c90da0022c3657199e133eeaa1d5d8486
Danilovir <danilo.vieira7@fatec.sp.gov.br>
2024-04-04 10:55:22 -0300

    3B3B

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import Title from './SRC/Components/Title';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     <Title/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

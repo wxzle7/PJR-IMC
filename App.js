27c9f3c586b45d24af86682a2d7e58a08aa7bcbd
Danilovir <danilo.vieira7@fatec.sp.gov.br>
2024-04-11 10:46:34 -0300

    Formulario 3B1 Lado B

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './SRC/Components/Title';
+import Main from './SRC/Components/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
      <Title/>
+     <Main/>
     </View>
   );
 }
---
+++ SRC/Components/Form/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+
+export default function Form(){
+    return(
+        <View>
+          
+          <View>
+            <Text>Altura</Text>
+            <TextInput
+             placeholder="Ex.1.75"
+             keyboardType="numeric"
+            />
+
+            <Text>Peso</Text>
+            <TextInput
+             placeholder="Ex. 85.766"
+             keyboardType="numeric"
+            />
+
+          </View>
+
+        </View>
+    );
+}
